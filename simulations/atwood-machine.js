13ebc7588edd388390a19fe699da8c15fb98278d
AsherMorgan <59518073+AsherMorgan@users.noreply.github.com>
2021-03-01 10:23:06 -0800

    Improve atwood machine simulation

--- simulations/atwood-machine.js
+++ simulations/atwood-machine.js
@@ -1,6 +1,7 @@
 const App = {
     data: function() {
         return {
+            angle: 90,          // The angle of the 2nd weight (degrees)
             mass1: 1,           // The mass of the 1st weight (Kg)
             mass2: 10,          // The mass of the 2nd weight (Kg)
             gravity: 9.8,       // The acceleration due to gravity (m/s/s)
@@ -12,35 +13,48 @@ const App = {
     },
     computed: {
         /**
-         * The net force on the 1st weight
+         * The net force on the 2nd weight
          */
         netForce: function() {
-            return (this.mass1 * this.gravity) - (this.mass2 * this.gravity);
+            let gravityX = this.getO(this.angle, this.mass2 * this.gravity);
+            let netX = (this.mass1 * this.gravity) - gravityX;
+            return netX;
         },
 
         /**
-         * The acceleration of the 1st weight
+         * The acceleration of the 2nd weight
          */
         acceleration: function() {
-            return this.netForce / (this.mass1 + this.mass2);
+            return this.netForce / (this.mass2 + this.mass1);
         },
 
         /**
-         * The velocity of the 1st weight
+         * The velocity of the 2nd weight
          */
         velocity: function() {
             return this.acceleration * this.time;
         },
 
         /**
-         * The heights of the weights
+         * The displacement of the 2nd weight
          */
-        positions: function() {
-            let position = 0.5 * this.acceleration * this.time * this.time;
-            if (position > 3) position = 3;
-            if (position < -3) position = -3;
-            if (Math.abs(position) === 3 && this.active) this.toggle();
-            return [position, -position];
+        displacement: function() {
+            let result = 0.5 * this.acceleration * this.time * this.time;
+            if (result > 3) result = 3;
+            if (result < -3) result = -3;
+            if (Math.abs(result) === 3 && this.active) this.toggle();
+            return result;
+        },
+
+        /**
+         * The position of the 2nd weight
+         */
+        positionVector: function() {
+            let x1 = this.getA(this.angle - 90, 1);
+            let y1 = this.getO(this.angle - 90, 1);
+            let x2 = this.getA(this.angle, - this.displacement + 3) + x1;
+            let y2 = this.getO(this.angle, - this.displacement + 3) + y1;
+            return [x1, y1, x2, y2];
         },
     },
     methods: {
@@ -65,6 +79,26 @@ const App = {
          */
         update: function() {
             this.time += this.refreshRate;
+        },
+
+        /**
+         * Get the length of the opposite side of a triangle
+         * @param {Number} angle The angle in degrees
+         * @param {Number} distance The length of the hypotenuse
+         * @returns {Number} The length of the opposite side
+         */
+        getO: function(angle, distance) {
+            return Math.sin(angle / 360 * 2 * Math.PI) * distance;
+        },
+
+        /**
+         * Get the length of the adjacent side of a triangle
+         * @param {Number} angle The angle in degrees
+         * @param {Number} distance The length of the hypotenuse
+         * @returns {Number} The length of the adjacent side
+         */
+        getA: function(angle, distance) {
+            return Math.cos(angle / 360 * 2 * Math.PI) * distance;
         },
     },
 }

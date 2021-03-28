9879bf0f9746feaed7498528b206bdb6a36ea947
AsherMorgan <59518073+AsherMorgan@users.noreply.github.com>
2021-03-28 15:54:27 -0700

    Add ability to zoom and pan projectile motion svg

--- simulations/projectile-motion.js
+++ simulations/projectile-motion.js
@@ -13,6 +13,7 @@ const App = {
             active: false,      // Whether the simulation is active
             refreshRate: 0.01,  // The simulation refresh rate (s)
             intervalId: null,   // The value returned by setInterval
+            svg: null,
         }
     },
     computed: {
@@ -105,10 +106,81 @@ const App = {
 // Create Vue app
 function createApp() {
     // Create app
-    Vue.createApp(App).mount("#app");
+    let app = Vue.createApp(App).mount("#app");
 
     // Unhide app divs
     document.getElementById("input").hidden = false;
     document.getElementById("output").hidden = false;
     document.getElementById("data").hidden = false;
+
+    // Enable zooming
+    let mobileZoomPanHandler = {
+        haltEventListeners: ["touchstart", "touchend", "touchmove", "touchleave", "touchcancel"],
+        init: function(options) {
+            var instance = options.instance;
+            let initialScale = 1;
+            let pannedX = 0;
+            let pannedY = 0;
+
+            // Init Hammer
+            // Listen only for pointer and touch events
+            this.hammer = Hammer(options.svgElement, {
+                inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+            });
+
+            // Enable pinch
+            this.hammer.get("pinch").set({enable: true});
+
+            // Handle double tap
+            this.hammer.on("doubletap", function(ev){
+                instance.zoomIn()
+            });
+
+            // Handle pan
+            this.hammer.on("panstart panmove", function(ev){
+                // On pan start reset panned variables
+                if (ev.type === "panstart") {
+                    pannedX = 0;
+                    pannedY = 0;
+                }
+
+                // Pan only the difference
+                instance.panBy({x: ev.deltaX - pannedX, y: ev.deltaY - pannedY});
+                pannedX = ev.deltaX;
+                pannedY = ev.deltaY;
+            })
+
+            // Handle pinch
+            this.hammer.on("pinchstart pinchmove", function(ev) {
+                // Calculate true zoom center
+                const el = ev.target;
+                const rect = el.getBoundingClientRect();
+                const pos = {
+                    x: (ev.center.x - rect.left),
+                    y: (ev.center.y - rect.top),
+                };
+
+                // On pinch start remember initial zoom
+                if (ev.type === "pinchstart") {
+                    initialScale = instance.getZoom();
+                    instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: pos.y});
+                }
+
+                instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: pos.y});
+            });
+
+            // Prevent moving the page on some devices when panning over SVG
+            options.svgElement.addEventListener("touchmove", function(e){ e.preventDefault(); });
+        },
+
+        destroy: function(){
+            this.hammer.destroy();
+        }
+    }
+    app.svg = svgPanZoom("svg", {
+        dblClickZoomEnabled: false,
+        minZoom: 0.1,
+        maxZoom: 10,
+        customEventsHandler: mobileZoomPanHandler,
+    });
 }

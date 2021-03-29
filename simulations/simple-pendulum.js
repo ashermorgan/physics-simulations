c67aafe3de02f141cdf453095bac2c9b67015112
AsherMorgan <59518073+AsherMorgan@users.noreply.github.com>
2021-03-28 19:15:25 -0700

    Add simple pendulum simulation

---
+++ simulations/simple-pendulum.js
@@ -0,0 +1,84 @@
+const App = {
+    data: function() {
+        return {
+            radius: 5,          // The radius of the pendulum (m)
+            initialAngle: 5,    // The initial angle of the pendulum (degrees)
+            gravity: 9.8,       // The acceleration due to gravity (m/s/s)
+            time: 0,            // The time (s)
+            active: false,      // Whether the simulation is active
+            refreshRate: 0.01,  // The simulation refresh rate (s)
+            intervalId: null,   // The value returned by setInterval
+        }
+    },
+    computed: {
+        /**
+         * The period of the pendulum
+         */
+        period: function() {
+            if (this.initialAngle === 0) return 0;
+            else return 2 * Math.PI * Math.sqrt(this.radius / this.gravity);
+        },
+
+        /**
+         * The angle of the pendulum
+         */
+        angle: function() {
+            if (this.initialAngle === 0) return 0;
+            else return this.initialAngle * Math.cos(this.time * 2 * Math.PI / this.period);
+        },
+
+        /**
+         * The position of the pendulum mass
+         */
+        position: function() {
+            return {
+                x: Math.sin(this.angle * 2 * Math.PI / 360) * this.radius,
+                y: -Math.cos(this.angle * 2 * Math.PI / 360) * this.radius,
+            };
+        },
+
+        /**
+         * The acceleration of the pendulum mass
+         */
+        acceleration: function() {
+            return -Math.sin(this.angle * 2 * Math.PI / 360) * this.gravity;
+        },
+    },
+    methods: {
+        /**
+         * Toggle whether the simulation is active
+         */
+        toggle: function() {
+            this.active = !this.active;
+            if (this.active) this.intervalID = setInterval(this.update, this.refreshRate * 1000);
+            else clearInterval(this.intervalID);
+        },
+
+        /**
+         * Reset the simulation
+         */
+        reset: function() {
+            this.time = 0;
+        },
+
+        /**
+         * Update the simulation output
+         */
+        update: function() {
+            this.time += this.refreshRate;
+        },
+    },
+}
+
+
+
+// Create Vue app
+function createApp() {
+    // Create app
+    Vue.createApp(App).mount("#app");
+
+    // Unhide app divs
+    document.getElementById("input").hidden = false;
+    document.getElementById("output").hidden = false;
+    document.getElementById("data").hidden = false;
+}
